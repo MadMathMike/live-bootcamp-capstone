0260a7fc3c7538dccb373ef88b8dc30c20a87c72
Michael Peterson <MadMathMike@gmail.com>
2025-09-15 20:43:35 -0600

    Support runtime switching of load balancing algorithm

--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -52,23 +52,36 @@ async fn forward(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = Arc::new(tokio::sync::RwLock::new(Pool::new(vec![
+        "localhost:3002".to_owned(),
+        "localhost:3001".to_owned(),
+        "localhost:3003".to_owned(),
+    ])));
+
+    let config_addr = SocketAddr::from(([127, 0, 0, 1], 2999));
+    let config_listener = TcpListener::bind(config_addr).await?;
+    let pool_clone = pool.clone();
+    tokio::task::spawn(async move {
+        // We start a loop to continuously accept incoming connections
+        loop {
+            let (_, _) = config_listener.accept().await.unwrap();
+            let mut pool = pool_clone.write().await;
+            pool.cycle_algorithm();
+            println!("{:?}", pool.algorithm);
+        }
+    });
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
 
     let client = client::legacy::Builder::new(TokioExecutor::new()).build(HttpConnector::new());
-
-    let mut pool = Pool::new(vec![
-        "localhost:3001".to_owned(),
-        "localhost:3002".to_owned(),
-        "localhost:3003".to_owned(),
-    ]);
 
     // We start a loop to continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
         let client = client.clone();
-        let host = pool.next().unwrap();
+        let host = pool.write().await.next().unwrap();
 
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
@@ -84,9 +97,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
+#[derive(Debug)]
+pub enum LoadBalancingAlgorithm {
+    RoundRobin,
+    LeastConnections,
+}
+
 pub struct Pool {
     round_robin_counter: usize,
     hosts: Vec<Arc<String>>,
+    algorithm: LoadBalancingAlgorithm,
 }
 
 impl Pool {
@@ -94,6 +114,18 @@ impl Pool {
         Self {
             round_robin_counter: 0,
             hosts: hosts.into_iter().map(|host| Arc::new(host)).collect(),
+            algorithm: LoadBalancingAlgorithm::RoundRobin,
+        }
+    }
+
+    pub fn cycle_algorithm(&mut self) {
+        match self.algorithm {
+            LoadBalancingAlgorithm::RoundRobin => {
+                self.algorithm = LoadBalancingAlgorithm::LeastConnections
+            }
+            LoadBalancingAlgorithm::LeastConnections => {
+                self.algorithm = LoadBalancingAlgorithm::RoundRobin
+            }
         }
     }
 }
@@ -106,26 +138,33 @@ impl Iterator for Pool {
             return None;
         }
 
-        // Round robin
-        // self.round_robin_counter += 1;
-        // let index = self.round_robin_counter % self.hosts.len();
-        // Some(self.hosts.get(index).unwrap().clone())
-
-        let mut least_connections_host: Option<Arc<String>> = None;
-        let mut least_connection_count = usize::MAX;
-        for host in self.hosts.iter() {
-            // This reference count doesn't actually contain the number of active connections
-            // to the host, but it correlates. As the host is cloned for each new request,
-            // the internal counter of the Arc will increment. And when the request completes,
-            // the clone is dropped decrementing the counter.
-            let connection_count = Arc::strong_count(host);
-
-            if connection_count < least_connection_count {
-                least_connection_count = connection_count;
-                least_connections_host = Some(host.clone());
+        let host = match self.algorithm {
+            LoadBalancingAlgorithm::RoundRobin => {
+                // Round robin
+                self.round_robin_counter += 1;
+                let index = self.round_robin_counter % self.hosts.len();
+                self.hosts.get(index).unwrap().clone()
             }
-        }
+            LoadBalancingAlgorithm::LeastConnections => {
+                let mut least_connections_host: Option<Arc<String>> = None;
+                let mut least_connection_count = usize::MAX;
+                for host in self.hosts.iter() {
+                    // This reference count doesn't actually contain the number of active connections
+                    // to the host, but it correlates. As the host is cloned for each new request,
+                    // the internal counter of the Arc will increment. And when the request completes,
+                    // the clone is dropped, which decrements.
+                    let connection_count = Arc::strong_count(host);
 
-        least_connections_host
+                    if connection_count < least_connection_count {
+                        least_connection_count = connection_count;
+                        least_connections_host = Some(host.clone());
+                    }
+                }
+
+                least_connections_host.unwrap()
+            }
+        };
+
+        Some(host)
     }
 }
