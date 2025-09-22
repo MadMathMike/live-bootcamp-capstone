51fcb0d1098268da2303500ac31a66499bf4865b
Michael Peterson <MadMathMike@gmail.com>
2025-09-21 20:44:40 -0600

    Ping hosts and move them from 'good' to 'bad' if the ping fails

--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -5,14 +5,18 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Duration;
 
+use http_body_util::Full;
+use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
+use hyper::{StatusCode, Uri};
 use hyper_util::client;
+use hyper_util::client::legacy::Client;
 use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use tokio::net::TcpListener;
 use tokio::sync::RwLock;
-use tokio::time::interval;
+use tokio::time::{interval, timeout};
 
 use crate::forwarding::forward;
 use crate::load_balancing::*;
@@ -25,15 +29,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         SocketAddr::from(([127, 0, 0, 1], 3003)),
     ])));
 
-    // TODO: when health checks are implemented, update the monitor_pool
-    // function to ping all the potential hosts at least once so we don't
-    // start serving traffic to unresponsive hosts.
     tokio::task::spawn(monitor_pool(pool.clone()));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
 
-    let client = client::legacy::Builder::new(TokioExecutor::new()).build(HttpConnector::new());
+    let client = client::legacy::Builder::new(TokioExecutor::new()).build_http();
 
     // We start a loop to continuously accept incoming connections
     loop {
@@ -62,12 +63,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn monitor_pool(pool: Arc<RwLock<Pool>>) {
+    // TODO: make this interval configurable
     let mut interval = interval(Duration::from_secs(10));
+    let client: Client<HttpConnector, Full<Bytes>> =
+        client::legacy::Builder::new(TokioExecutor::new()).build_http();
 
     loop {
         interval.tick().await;
 
-        // TODO: Check host health via /ping endpoint and remove from pool if no response
+        let mut responsive_hosts = vec![];
+
+        // To allow proxy requests to continue while we ping the hosts, we get
+        // the host addresses from the pool on separate line to ensure the lock
+        // acquired as an intermediary value is dropped quickly.
+        let host_addresses = pool.read().await.into_iter();
+
+        // TODO: Ping all the hosts concurrently instead of serially
+        for address in host_addresses {
+            let uri = Uri::try_from(format!("http://{address}/ping")).unwrap();
+
+            // TODO: make this timeout configurable
+            let ping_response = timeout(Duration::from_millis(50), client.get(uri)).await;
+            match ping_response {
+                Ok(Ok(response)) if response.status() == StatusCode::OK => {
+                    responsive_hosts.push(address)
+                }
+                _ => println!("Failed to ping {}", address),
+            };
+        }
+
+        pool.write().await.set_good_hosts(&responsive_hosts);
 
         let algorithm = pool.write().await.determine_algorithm();
 
